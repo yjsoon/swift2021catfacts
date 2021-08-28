eb8ca202cd22b1182d3c3f19c5a85658fb44ca7c
YJ Soon <yjsoon@tinkertanker.com>
2021-08-28 11:35:06 +0800

    Initial Commit

---
+++ Cat Facts App/Cat_Facts_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_Facts_AppApp.swift
+//  Cat Facts App
+//
+//  Created by YJ Soon on 28/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Cat_Facts_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
